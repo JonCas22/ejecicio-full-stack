38f31336547f3e5c647ef802e0bbffa8754212b4
CristianPuga <58120121+CristianPuga@users.noreply.github.com>
2020-01-21 19:08:42 +0100

    publication, database, service, controller

---
+++ backend/src/publication/publication.entity.ts
@@ -0,0 +1,40 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+
+export class PublicationEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column({ length: 500 })
+    titulo: string;
+    @Column('boolean')
+    main: boolean;
+    @Column('text')
+    tipo: string;
+    @Column('text')
+    url: string;
+    @Column('boolean')
+    publicado: boolean;
+    @Column('boolean')
+    publico: boolean;
+    @Column('text')
+    autor: string;
+    @Column('text')
+    ultimo_editor: string;
+    @Column('text')
+    entradilla: string;
+    @Column('text')
+    imagen_portada: string;
+    @Column('text')
+    texto_completo: string;
+    @Column('text')
+    tags: string;
+    @Column('text')
+    version: string;
+    @Column('date')
+    fecha_creacion: Date;
+    @Column('date')
+    ultima_fecha_modificacion: Date;
+   
+   constructor(){}
+}
