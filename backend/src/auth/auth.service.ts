43148a155d46c33b736e53371300f8201a95a6ac
CristianPuga <58120121+CristianPuga@users.noreply.github.com>
2020-01-22 19:33:36 +0100

    errores

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -11,8 +11,8 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
+    if (user && user.contrasena === pass) {
+      const { contrasena, ...result } = user;
       return result;
     }
     return null;
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
 
 export type User = any;
 
@@ -6,7 +9,10 @@ export type User = any;
 export class UsersService {
   private readonly users: User[];
 
-  constructor() {
+  constructor(
+      @InjectRepository(UserEntity)
+      private readonly userRepository: Repository<UserEntity>
+  ) {
     this.users = [
       {
         userId: 1,
@@ -26,7 +32,9 @@ export class UsersService {
     ];
   }
 
-  async findOne(username: string): Promise<User> {
+  
+  async findOne(username: string): Promise<UserEntity> {
     return this.users.find(user => user.username === username);
   }
+  
 }
