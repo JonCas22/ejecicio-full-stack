fd650b0ead60a4376de1919b1f4e8e0b5658ad97
CristianPuga <58120121+CristianPuga@users.noreply.github.com>
2020-01-23 17:20:16 +0100

    errores metodos

--- backend/dist/user/user/user.controller.d.ts
+++ backend/dist/user/user/user.controller.d.ts
@@ -4,8 +4,8 @@ export declare class UserController {
     private readonly userService;
     constructor(userService: UserService);
     findAll(): Promise<UserEntity[]>;
-    create(photo: UserEntity): Promise<UserEntity>;
+    create(user: UserEntity): Promise<UserEntity>;
     findOne(id: number): Promise<UserEntity>;
-    update(id: number, photo: UserEntity): Promise<UserEntity>;
+    update(id: number, user: UserEntity): Promise<UserEntity>;
     remove(id: number): Promise<UserEntity>;
 }
